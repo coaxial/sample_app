a40aaa714ccbb258a8d2e121f3701c1f5d208a07
Pierre-Yves Poujade <py@poujade.org>
2014-04-16 14:17:33 -0400

    Write specs for error + rewrite flash div

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -19,7 +19,6 @@ describe "UserPages" do
   end
 
   describe "signup" do
-
     before { visit signup_path }
 
     let(:submit) { "Create my account" }
@@ -29,8 +28,7 @@ describe "UserPages" do
         expect { click_button submit }.not_to change(User, :count)
       end
 
-      describe "should show error messages" do
-        
+      describe "after submitting the form" do
         before { click_button submit }
         
         it { should have_content 'Sign up' }
@@ -51,9 +49,20 @@ describe "UserPages" do
         fill_in "Password",              with: "foobar"
         fill_in "Password confirmation", with: "foobar"
       end
+
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_selector('div.alert.alert-success', text: 'Welcome to the Sample App!') }
+        it { should have_title(user.name) }
+      end
+
+
     end
   end
 end
