cbb1c701b2f8ff913d34bcaade3b6e35e1378539
Pierre-Yves Poujade <py@poujade.org>
2014-04-18 16:32:51 -0400

    Add session support (login)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email])
-    if user && authenticate(params[:session][:password])
-      # Sign the user in
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_to user
     else
       flash.now[:error] = 'Invalid email/password combination.' # Using flash.now vs flash makes the flash message disapear after render. Otherwise the flash persists on the next page.
       render 'new'
