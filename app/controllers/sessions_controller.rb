f143ccb37e8e568f3ccbd0987e5cf5593f968238
Pierre-Yves Poujade <py@poujade.org>
2014-04-18 16:32:51 -0400

    3 tests fail. Why?

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,9 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email])
-    if user && authenticate(params[:session][:password])
-      # Sign the user in
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_to user
     else
       flash.now[:error] = 'Invalid email/password combination.' # Using flash.now vs flash makes the flash message disapear after render. Otherwise the flash persists on the next page.
       render 'new'
